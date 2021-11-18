52c8ef186d51483b74d8ec27d3c655ea9e275861
harshayerasi <harshayerasi@gmail.com>
2021-11-18 21:41:34 +0530

    Added low level design scalar

--- sidebars.js
+++ sidebars.js
@@ -5,54 +5,71 @@ module.exports = {
       id: "index",
     },
     {
-      type: 'category',
-      label: 'Artificial Intelligence',
+      type: "category",
+      label: "Artificial Intelligence",
       items: [
         {
-          type: 'category',
-          label: 'Statistics Essentials for Data science',
-          items: ['artificial-intelligence/statistics-essentials-for-data-science/estimators-and-estimates',
-                  'artificial-intelligence/statistics-essentials-for-data-science/hypothesis-testing',
-                  'artificial-intelligence/statistics-essentials-for-data-science/regression-analysis']
+          type: "category",
+          label: "Statistics Essentials for Data science",
+          items: [
+            "artificial-intelligence/statistics-essentials-for-data-science/estimators-and-estimates",
+            "artificial-intelligence/statistics-essentials-for-data-science/hypothesis-testing",
+            "artificial-intelligence/statistics-essentials-for-data-science/regression-analysis",
+          ],
         },
         {
-          type: 'category',
-          label: 'Data science with Python',
-          items: ['artificial-intelligence/data-science-with-python/session1',
-                  'artificial-intelligence/data-science-with-python/session2',
-                  'artificial-intelligence/data-science-with-python/session3',
-                  'artificial-intelligence/data-science-with-python/numpy']
-        }
+          type: "category",
+          label: "Data science with Python",
+          items: [
+            "artificial-intelligence/data-science-with-python/session1",
+            "artificial-intelligence/data-science-with-python/session2",
+            "artificial-intelligence/data-science-with-python/session3",
+            "artificial-intelligence/data-science-with-python/numpy",
+          ],
+        },
       ],
     },
     {
-      type: 'category',
-      label: 'Azure',
+      type: "category",
+      label: "Azure",
       items: [
         {
-          type: 'category',
-          label: 'AZ 900',
-          items: ['azure/az-900/azure-fundamentals-cloud-computing']
-        }
+          type: "category",
+          label: "AZ 900",
+          items: ["azure/az-900/azure-fundamentals-cloud-computing"],
+        },
       ],
     },
     {
-      type: 'category',
-      label: 'Kubernetes',
-      items: ['kubernetes/kubernetes-fundamentals']
-    },
-    {
-      type: 'category',
-      label: 'React',
-      items: ['react/react-introduction']
-    },
-    {
-      type: 'category',
-      label: 'React Native',
+      type: "category",
+      label: "Java",
       items: [
-          'react-native/react-native-introduction',
-          'react-native/react-native-commands'  
-        ]
+        {
+          type: "category",
+          label: "Low level design",
+          items: [
+            "programming-languages/java/low-level-design/low-level-design-101",
+          ],
+        },
+      ],
+    },
+    {
+      type: "category",
+      label: "Kubernetes",
+      items: ["kubernetes/kubernetes-fundamentals"],
+    },
+    {
+      type: "category",
+      label: "React",
+      items: ["react/react-introduction"],
+    },
+    {
+      type: "category",
+      label: "React Native",
+      items: [
+        "react-native/react-native-introduction",
+        "react-native/react-native-commands",
+      ],
     },
     {
       type: "doc",
@@ -65,6 +82,6 @@ module.exports = {
     {
       type: "doc",
       id: "tools",
-    }
-  ]
+    },
+  ],
 };
